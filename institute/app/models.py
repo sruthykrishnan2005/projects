83fe52576c78deccb8b4cc470c032648f7252dc6
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-11-25 17:53:29 +0530

    instituteadded

--- institute/app/models.py
+++ institute/app/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 class Course(models.Model):
-    """
-    Model for storing navigation bar links.
-    """
     cname = models.TextField()
     dis=models.TextField()
     img=models.FileField()
